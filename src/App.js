e2f84f700113953974714d92ab50076e79dd72b7
uwebury <142432719+uwebury@users.noreply.github.com>
2024-06-13 15:19:33 +0200

    Add ParticlesBg

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import ParticlesBg from "particles-bg";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceFinder /> */}
-    </div>
+    <>
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceFinder /> */}
+      </div>
+      <ParticlesBg type="cobweb" bg={true} />
+    </>
   );
 }
 
