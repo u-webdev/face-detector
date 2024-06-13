7c94833973f22a9af47fa250ca8b65367d9dece6
uwebury <142432719+uwebury@users.noreply.github.com>
2024-06-13 14:24:32 +0200

    Add Tilt

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
 
 const Logo = () => {
-  return <div className="ma4 mt0 "></div>;
+  return (
+    <div className="ma4 mt0 br2 shadow-2" style={{ width: "150px" }}>
+      <Tilt>
+        <div
+          style={{
+            height: "150px",
+            backgroundColor: "darkgreen",
+          }}
+        >
+          <h1>React Parallax Tilt 👀</h1>
+        </div>
+      </Tilt>
+    </div>
+  );
 };
 
 export default Logo;
