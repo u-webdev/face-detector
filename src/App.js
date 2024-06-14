3f1fc2d937dc30fd3f06371cbd45db61a446cc23
uwebury <142432719+uwebury@users.noreply.github.com>
2024-06-14 16:08:37 +0200

    Add onRouteChange arguments

--- src/App.js
+++ src/App.js
@@ -89,14 +89,14 @@ class App extends Component {
     });
   };
 
-  onRouteChange = () => {
-    this.setState({ route: "home" });
+  onRouteChange = (route) => {
+    this.setState({ route: route });
   };
 
   render() {
     return (
       <div className="App">
-        <Navigation />
+        <Navigation onRouteChange={this.onRouteChange} />
         {this.state.route === "signin" ? (
           <SignIn onRouteChange={this.onRouteChange} />
         ) : (
