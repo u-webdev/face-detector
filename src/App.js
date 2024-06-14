6913605a582c72d1d4c0f50d3b0a8bf8d516d9c0
uwebury <142432719+uwebury@users.noreply.github.com>
2024-06-14 10:53:21 +0200

    Refactor App component from function to class component

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import ParticlesBg from "particles-bg";
+import { Component } from "react";
 
-function App() {
-  return (
-    <>
+class App extends Component {
+  render() {
+    return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
         <ImageLinkForm />
         {/* <FaceFinder /> */}
+        <ParticlesBg type="cobweb" color="#D3D3D3" bg={true} />
       </div>
-      <ParticlesBg type="cobweb" color="#D3D3D3" bg={true} />
-    </>
-  );
+    );
+  }
 }
 
 export default App;
