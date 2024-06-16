5e8fdbc7afbdf2114c7b6c0f42f59f6e3d192868
uwebury <142432719+uwebury@users.noreply.github.com>
2024-06-16 16:56:22 +0200

    Prop drilling

--- src/App.js
+++ src/App.js
@@ -130,7 +130,10 @@ class App extends Component {
         {route === "home" ? (
           <div>
             <Logo />
-            <Rank />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -138,7 +141,7 @@ class App extends Component {
             <FaceDetection boxes={boxes} imageUrl={imageUrl} />
           </div>
         ) : this.state.route === "signin" ? (
-          <SignIn onRouteChange={this.onRouteChange} />
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         ) : (
           <Register
             loadUser={this.loadUser}
