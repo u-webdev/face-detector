a4e07fe23915102ee37171cffbcd5aa2ee8b6ca2
uwebury <142432719+uwebury@users.noreply.github.com>
2024-06-16 15:03:15 +0200

    Create stats signInEmail and signInPassword

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,6 +1,22 @@
 import React from "react";
 
 class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: "",
+      signInPassword: "",
+    };
+  }
+
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  };
+
   render() {
     const { onRouteChange } = this.props;
     return (
